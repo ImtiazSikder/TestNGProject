a1534b20567a7ca4c4ffcaf2786c178dfcf08a56
ImtiazSikder <119623454+ImtiazSikder@users.noreply.github.com>
2023-11-21 23:17:55 -0500

    Update LoginTest.java

--- src/test/java/com/testng/LoginTest.java
+++ src/test/java/com/testng/LoginTest.java
@@ -4,6 +4,7 @@ public class LoginTest {
 	
 	public static void main(String [] args) {
 		System.out.println("This is Login Test");
+		Sysotem.out.println("Next time real code will be added");
 	}
 
 }
